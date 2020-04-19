d0a5fa510c58a9e7067404b40864e248db57c38b
Kamran Javed <kamranjaved1234@gmail.com>
2020-04-19 15:37:16 +0530

    setting route error removed

--- routes/settingRoutes.js
+++ routes/settingRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const settingController = require("./../controller/settingController")''
+const settingController = require("./../controller/settingController");
 const authController = require("./../controller/authController");
 
 const router = express.Router({ mergeParams: true });
